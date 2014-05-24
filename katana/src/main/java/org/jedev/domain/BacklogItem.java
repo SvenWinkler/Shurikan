81648bb63fb2e974922273f30553db1ce112bbc6
svnwnk <svenwink@gmx.de>
2014-05-24 18:18:13 +0200

    added properties for backlogitem

--- katana/src/main/java/org/jedev/domain/BacklogItem.java
+++ katana/src/main/java/org/jedev/domain/BacklogItem.java
@@ -1,7 +1,19 @@
 package org.jedev.domain;
 
+import java.math.BigInteger;
+
 /**
  * Created by svnwnk on 24/05/14.
  */
 public interface BacklogItem {
+
+	String getName();
+	void setName( String name );
+
+	String getDescription();
+	void setDescription( String description );
+
+	BigInteger getStackRank();
+	void setStackRank( BigInteger stackRank );
+
 }
