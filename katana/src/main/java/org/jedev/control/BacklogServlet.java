8e822e5256d4c84c1435b7196d51bd873d0380b4
svnwnk <svenwink@gmx.de>
2014-05-26 12:06:33 +0200

    Kill Boris 2

--- katana/src/main/java/org/jedev/control/BacklogServlet.java
+++ katana/src/main/java/org/jedev/control/BacklogServlet.java
@@ -17,7 +17,7 @@ import java.io.IOException;
 public class BacklogServlet extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
 
 		resp.getWriter().write("Backlog");
 	}
